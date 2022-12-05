fc600b610e9ddf1bd76fbd42e1a83431652d6f2d
Maximiliano Alonso <84605497+MaximilianoAlonso@users.noreply.github.com>
2022-12-05 13:42:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path')
 
-const express = require("express");
-const app = express()
-const path = require("path")
-const port = 3030
+app.use(express.static('public'))
 
+app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/babbage.html',(req, res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')));
+app.get('/berners-lee.html',(req, res) => res.sendFile(path.join(__dirname, 'views', 'berners-lee.html')));
+app.get('/clarke.html',(req, res) => res.sendFile(path.join(__dirname, 'views', 'clarke.html')));
+app.get('/hamilton.html',(req, res) => res.sendFile(path.join(__dirname, 'views', 'hamilton.html')));
+app.get('/hopper.html',(req, res) => res.sendFile(path.join(__dirname, 'views', 'hopper.html')));
+app.get('/lovelace.html',(req, res) => res.sendFile(path.join(__dirname, 'views', 'lovelace.html')));
+app.get('/turing.html',(req, res) => res.sendFile(path.join(__dirname, 'views', 'turing.html')));
+app.get('*',(req, res) => res.sendFile(path.join(__dirname, 'views', 'error404.html')));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname,"views","index.html")));
-app.get("/babbage", (req, res) => res.sendFile(path.join(__dirname,"views","babbage.html")))
-app.get("/berners-lee", (req, res) => res.sendFile(path.join(__dirname,"views","berners-lee.html")))
-app.get("/hamilton", (req, res) => res.sendFile(path.join(__dirname,"views","hamilton.html")))
-app.get("/hopper", (req, res) => res.sendFile(path.join(__dirname,"./views","hopper.html")))
-app.get("/lovelace", (req, res) => res.sendFile(path.join(__dirname,"views","lovelace.html")))
-app.get("/clarke", (req, res) => res.sendFile(path.join(__dirname,"views","clarke.html")))
-app.get("/turing", (req, res) => res.sendFile(path.join(__dirname,"views","turing.html")))
-app.get("*", (req, res) => res.sendFile(path.join(__dirname,"views","error404.html")))
+app.listen(port, () => console.log(`Servidor funcionando en http://localhost:${port}`));
 
-
-app.listen(port, () => console.log("server iniciado en puerto 3030"))
